12da62bb60bb8862ce68362dd599fa705359710b
Fran Barton <francis.barton@nuh.nhs.uk>
2023-11-13 17:37:10 +0000

    Add data columns to sf table, rather than vice versa

--- R/map_imd_icb.R
+++ R/map_imd_icb.R
@@ -3,53 +3,48 @@
 #' @param icb_name character. The name of the ICB to map.
 #' @param include_title logical. Decide whether to include the plot title. Default = TRUE
 #'
-#' @return list. A list containing a ggplot object, and the name of the ICB.
+#' @returns list. A list containing a ggplot object, and the name of the ICB.
 #' @export
-#'
-map_imd_icb <- function(icb_name, include_title = TRUE){
-
-  # need this call here to prevent some strange errors later
-
-  library(sf)
+map_imd_icb <- function(icb_name, include_title = TRUE) {
 
   # get the IMD data
   imd <- IMD::imd_england_lsoa |>
-    dplyr::mutate(
-      IMD_decile = factor(IMD_decile)
-    )
+    dplyr::mutate(across("IMD_decile", as.factor)) |>
+    dplyr::rename(lsoa11cd = "lsoa_code")
 
   # get the shapefile data we need
-  geometry <- geographr::boundaries_lsoa11
+  lsoa_bounds <- geographr::boundaries_lsoa11 |>
+    dplyr::rename(
+      lsoa11nm = "lsoa11_name",
+      lsoa11cd = "lsoa11_code"
+    )
+
+  lsoa_bounds_valid <- lsoa_bounds |>
+    sf::st_make_valid()
 
   # lookup between lsoa and icb
   lookup <- geographr::lookup_lsoa11_sicbl22_icb22_ltla22 |>
-    dplyr::select(lsoa11_code, icb22_name)
+    dplyr::select(all_of(c(lsoa11cd = "lsoa11_code", "icb22_name"))) |>
+    dplyr::filter(if_any("icb22_name", \(x) x == {{ icb_name }}))
 
-  # join the geometry and imb data on
-  dtf <- lookup |>
-    dplyr::filter(icb22_name == icb_name) |>
-    dplyr::left_join(geometry, by = "lsoa11_code") |>
-    dplyr::left_join(imd, by = c("lsoa11_code" = "lsoa_code"))
+  # join the lookup to the boundary data, and then join IMD data on
+  # (it's always better to start with an sf table and add data columns to that)
+  dtf <- lsoa_bounds_valid |>
+    dplyr::inner_join(lookup, "lsoa11cd") |>
+    dplyr::left_join(imd, "lsoa11cd")
 
-  # decide what title to use
-  plot_title <- ifelse(include_title, icb_name, "")
+  # create a custom palette (magenta to white)
+  imd_palette <- grDevices::colorRampPalette(c("#71196E", "#FFFFFF"))(10)
 
   # make the plot
-  plt <- ggplot2::ggplot(dtf) +
-    ggplot2::geom_sf(ggplot2::aes(geometry = geometry, fill = IMD_decile)) +
-    ggplot2::scale_fill_manual(values = colorRampPalette(c("#71196E", "#FFFFFF"))(10)) +
+  dtf |>
+    ggplot2::ggplot() +
+    ggplot2::geom_sf(aes(fill = dtf[["IMD_decile"]])) +
+    ggplot2::scale_fill_manual(values = imd_palette) +
     ggplot2::theme_void() +
     ggplot2::labs(
-      title = plot_title,
+      title = ifelse(include_title, icb_name, ""),
       fill = "IMD decile",
       caption = "Data Source: MHCLG"
     )
-
-  # create a list to support Rmd output of title
-  list(
-    name = icb_name,
-    plot = plt
-  )
-
 }
-
